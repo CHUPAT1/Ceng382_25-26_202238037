f647cfd1daab2a5cebce5cc5f497a3ce7a1201f8
CHUPAT1 <cagatayyuksel5561@gmail.com>
2025-04-04 19:55:05 +0300

    Made the instructions and made a dark theme

---
+++ LabProject/Models/ClassInformationModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LabProject.Models {
+    public class ClassInformationModel {
+        public int Id { get; set; }
+
+        [Required]
+        public string ClassName { get; set; }
+
+        [Required]
+        public int StudentCount { get; set; }
+
+        public string Description { get; set; }
+    }
+}
